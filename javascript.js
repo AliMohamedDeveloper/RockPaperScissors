ef9470a52dd8b5203d18b83c5b00fa90703ff8b3
Ali Mohamed <225834754+AliMohamedDeveloper@users.noreply.github.com>
2025-08-28 19:26:56 -0400

    Finshed step 2 by creating a getComputerCHoice fuction that randomly returns rock paper or scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let ran = Math.floor(Math.random() * 3);
+
+    if (ran == 0) {
+        return "Rock";
+    }
+
+    else if (ran == 1) {
+        return "Paper";
+    }
+
+    else {
+        return "Scissors";
+    }
+}
+
+
+console.log(getComputerChoice());
