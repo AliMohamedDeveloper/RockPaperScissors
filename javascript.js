fb38f46c50810d94e1c986a5d8f01c784c8823e6
Ali Mohamed <225834754+AliMohamedDeveloper@users.noreply.github.com>
2025-08-28 20:33:05 -0400

    Completed step 4 and 5 by finishing playRound code and tracking scores for human and computer choices

--- javascript.js
+++ javascript.js
@@ -2,24 +2,63 @@ function getComputerChoice() {
     let ran = Math.floor(Math.random() * 3);
 
     if (ran == 0) {
-        return "Rock";
+        return "rock";
     }
 
     else if (ran == 1) {
-        return "Paper";
+        return "paper";
     }
 
     else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Enter 'Rock' , 'Paper' or 'Scissors'");
+    let Choice = prompt("Enter 'Rock' , 'Paper' or 'Scissors'");
 
-    return humanChoice;
+    return Choice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice) {
+
+    let humanChoiceLC = humanChoice.toLowerCase();
+    let humanScore = 0;
+    let = 0;
+    if (humanChoice == 'rock' && computerChoice == 'paper') {
+        computerScore += 1;
+    }
+
+    else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        humanScore += 1;
+    }
+
+    else if (computerChoice == 'rock' && humanChoice == 'paper') {
+        humanScore += 1;
+    }
+
+    else if (computerChoice == 'rock' && humanChoice == 'scissors') {
+        computerScore += 1;
+    }
+    else {
+        console.log("Draw");
+    }
+
+    console.log("human score " + humanScore);
+    console.log("Computer score " + computerScore);
 }
 
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+let GCC = (getComputerChoice());
+let GHC = (getHumanChoice());
+
+
+console.log("Human Choice is " + GHC);
+console.log("Computer Score is " + GCC);
+
+playRound(GHC, GCC);
+
